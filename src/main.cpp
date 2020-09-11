d92292832fbfc630e8f058738878a50a786b90a4
Ryan McDonnell <ryan@binaryfactory.com>
2020-09-10 17:55:13 -0700

    Driving LEDs with two pots

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,85 @@
 #include <Arduino.h>
-#include <HID-Project.h>
+#include <SPI.h>
 
-int pinA = 2;
-int pinB = 4;
-int volume = 0;
+const byte address = 0x00;
+const int encoderA = PIN2;
+const int encoderB = PIN4;
+const int encoderSwitch = PIN3;
+const int leftPad = PIN5;
+const int rightPad = PIN6;
+
+const int volumeUpOhms = 1000; // 1k
+const int volumeDownOhms = 3200; // 3.2k
+const int potMaxSteps = 128;
+const int potMaxOhms = 10000; // 10k
+
+const int volUpStepValue = (volumeUpOhms / potMaxOhms) * potMaxSteps;
+const int volDownStepValue = (volumeDownOhms / potMaxOhms) * potMaxSteps;
+const int muteStepValue = 0;
+
+void digitalPotWrite(int padCS, int value)
+{
+  digitalWrite(padCS, LOW);
+  
+  // Button pressed
+  SPI.transfer(address);
+  SPI.transfer(value);
+  
+  delay(100);
+  
+  // Button released
+  //SPI.transfer(address);
+  //SPI.transfer(potMaxSteps);
+
+  digitalWrite(padCS, HIGH);
+}
 
 void encoderTurned() {
-  int valA = digitalRead(pinA);
-  int valB = digitalRead(pinB);
+  int valA = digitalRead(encoderA);
+  int valB = digitalRead(encoderB);
+  int r = 0;
   
   // Compare pins to determine in which direction encoder was turned
   if (valA != valB) {
       // Clockwise
-      volume++;
-      Consumer.write(MEDIA_VOL_UP);
+      digitalPotWrite(leftPad, volUpStepValue);
   }
   else {
       // Counter-clockwise
-      volume--;
-      Consumer.write(MEDIA_VOL_DOWN);
+      digitalPotWrite(leftPad, volDownStepValue);
   }
+}
 
-  Serial.print("Volume: ");
-  Serial.println(volume);
+void encoderSwitchPushed() {
+  digitalPotWrite(rightPad, muteStepValue);
 }
 
 void setup() {
-  Serial.begin(115200);
+  pinMode(encoderA, INPUT);  
+  pinMode(encoderB, INPUT);
+  pinMode(encoderSwitch, INPUT);
 
-  pinMode(pinA, INPUT);  
-  pinMode(pinB, INPUT);
+  pinMode(leftPad, OUTPUT);
+  pinMode(rightPad, OUTPUT);
 
-  attachInterrupt(0, encoderTurned, RISING); // Only trigger in high position
+  SPI.begin();
 
-  Consumer.begin();
+  attachInterrupt(digitalPinToInterrupt(encoderA), encoderTurned, RISING); // Only trigger in high position
+  attachInterrupt(digitalPinToInterrupt(encoderSwitch), encoderSwitchPushed, RISING);
 }
 
-void loop() {}
+void loop() {
+  for (int i = 50; i <= 128; i++)
+  {
+    digitalPotWrite(leftPad, i);
+    digitalPotWrite(rightPad, i);
+    delay(10);
+  }
+
+  for (int i = 128; i >= 50; i--)
+  {
+    digitalPotWrite(leftPad, i);
+    digitalPotWrite(rightPad, i);
+    delay(10);
+  } 
+}
